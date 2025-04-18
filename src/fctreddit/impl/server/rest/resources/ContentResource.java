3f5e3426773c2dff4e623923fa5d1e8eb3d3f650
Miguel Lourenço <mse.lourenco@campus.fct.unl.pt>
2025-04-18 23:24:12 +0100

    Add structures for Content Commands.

--- src/fctreddit/impl/server/rest/resources/ContentResource.java
+++ src/fctreddit/impl/server/rest/resources/ContentResource.java
@@ -1,5 +1,94 @@
 package fctreddit.impl.server.rest.resources;
 
-public class ContentResource {
+import fctreddit.api.Post;
+import fctreddit.api.java.Content;
+import fctreddit.api.rest.RestContent;
+import fctreddit.impl.server.java.JavaContent;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class ContentResource implements RestContent {
+
+    private static Logger Log = Logger.getLogger(ContentResource.class.getName());
+
+    private Content cont;
+
+    public ContentResource(Content cont) {
+        //hibernate = Hibernate.getInstance();
+        cont = new JavaContent();
+    }
+
+    @Override
+    public String createPost(Post post, String userPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createPost'");
+    }
+
+    @Override
+    public List<String> getPosts(long timestamp, String sortOrder) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosts'");
+    }
+
+    @Override
+    public Post getPost(String postId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPost'");
+    }
+
+    @Override
+    public List<String> getPostAnswers(String postId, long timeout) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPostAnswers'");
+    }
+
+    @Override
+    public Post updatePost(String postId, String userPassword, Post post) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updatePost'");
+    }
+
+    @Override
+    public void deletePost(String postId, String userPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletePost'");
+    }
+
+    @Override
+    public void upVotePost(String postId, String userId, String userPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'upVotePost'");
+    }
+
+    @Override
+    public void removeUpVotePost(String postId, String userId, String userPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeUpVotePost'");
+    }
+
+    @Override
+    public void downVotePost(String postId, String userId, String userPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'downVotePost'");
+    }
+
+    @Override
+    public void removeDownVotePost(String postId, String userId, String userPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeDownVotePost'");
+    }
+
+    @Override
+    public Integer getupVotes(String postId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getupVotes'");
+    }
+
+    @Override
+    public Integer getDownVotes(String postId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDownVotes'");
+    }
 
 }
