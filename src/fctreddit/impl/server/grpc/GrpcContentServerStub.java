678b25b04b8e9a54bed41e279a359dc274ae19f7
Miguel Lourenço <mse.lourenco@campus.fct.unl.pt>
2025-04-21 15:11:05 +0100

    Add Grpc servers and serverstubs (incomplete).

--- src/fctreddit/impl/server/grpc/GrpcContentServerStub.java
+++ src/fctreddit/impl/server/grpc/GrpcContentServerStub.java
@@ -1,5 +1,82 @@
 package fctreddit.impl.server.grpc;
 
-public class GrpcContentServerStub {
+import fctreddit.api.java.Content;
+import fctreddit.impl.grpc.generated_java.ContentGrpc;
+import fctreddit.impl.grpc.generated_java.ContentProtoBuf.*;
+import io.grpc.stub.StreamObserver;
+import fctreddit.impl.server.java.JavaContent;
+import io.grpc.BindableService;
+import io.grpc.ServerServiceDefinition;
+
+public class GrpcContentServerStub implements ContentGrpc.AsyncService, BindableService {
+    
+    Content impl = new JavaContent();
+
+    @Override
+    public ServerServiceDefinition bindService() {
+        return ContentGrpc.bindService(this);
+    }
+
+    @Override
+    public void createPost(CreatePostArgs request, StreamObserver<CreatePostResult> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createPost'");
+    }
+    @Override
+    public void getPosts(GetPostsArgs request, StreamObserver<GetPostsResult> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosts'");
+    }
+    @Override
+    public void getPost(GetPostArgs request, StreamObserver<GrpcPost> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPost'");
+    }
+    @Override
+    public void getPostAnswers(GetPostAnswersArgs request, StreamObserver<GetPostsResult> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPostAnswers'");
+    }
+    @Override
+    public void updatePost(UpdatePostArgs request, StreamObserver<  GrpcPost> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updatePost'");
+    }
+    @Override
+    public void deletePost(DeletePostArgs request, StreamObserver<EmptyMessage> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletePost'");
+    }
+    @Override
+    public void upVotePost(ChangeVoteArgs request, StreamObserver<EmptyMessage> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'upVotePost'");
+    }
+    @Override
+    public void removeUpVotePost(ChangeVoteArgs request, StreamObserver<EmptyMessage> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeUpVotePost'");
+    }
+    @Override
+    public void downVotePost(ChangeVoteArgs request, StreamObserver<EmptyMessage> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'downVotePost'");
+    }
+    @Override
+    public void removeDownVotePost(ChangeVoteArgs request, StreamObserver<EmptyMessage> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeDownVotePost'");
+    }
+    @Override
+    public void getUpVotes(GetPostArgs request, StreamObserver<VoteCountResult> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUpVotes'");
+    }
+    @Override
+    public void getDownVotes(GetPostArgs request, StreamObserver<VoteCountResult> responseObserver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDownVotes'");
+    }
+
 
 }
